a1ec7d2a7da61ef2296b986353f65e2f7b1ebae7
eswar-sudo <eswarkumar501@gmail.com>
2025-07-25 19:48:17 +0530

    Create aws-auth.tf

---
+++ modules/aws-auth.tf
@@ -0,0 +1,24 @@
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = aws_iam_role.karpenter_node.arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = [
+          "system:bootstrappers",
+          "system:nodes"
+        ]
+      }
+    ])
+  }
+
+  lifecycle {
+    ignore_changes = [data]
+  }
+}
